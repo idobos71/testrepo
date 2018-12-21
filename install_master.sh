d592885e8e8591a96f9472fcf58047714faf4da9
Ivan Dobos <ivan.dobos@canonical.com>
2018-12-21 22:18:50 +0100

    kubernetes.list now fixed

--- install_master.sh
+++ install_master.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get install -y docker.io
 sudo bash -c 'echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list'
+sudo bash -c 'curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg |apt-key add -'
 sudo apt-get update
 sudo apt-get install -y kubeadm kubectl kubelet
 sudo kubeadm init --pod-network-cidr 192.168.0.0/16
