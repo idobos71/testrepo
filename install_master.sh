d487f24edecd1e26ae9048d88e1f00ab417a26de
Ivan Dobos <ivan.dobos@canonical.com>
2018-12-22 00:35:50 +0100

    Adding rbac.yaml and calico.yaml with 192.169.0.0/16 CIDR

--- install_master.sh
+++ install_master.sh
@@ -4,19 +4,15 @@ sudo bash -c 'echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc
 sudo bash -c 'curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg |apt-key add -'
 sudo apt-get update
 sudo apt-get install -y kubeadm kubectl kubelet
-sudo kubeadm init --pod-network-cidr 192.168.0.0/16
+sudo kubeadm init --pod-network-cidr 192.169.0.0/16
 echo "2 minutky cakania ..."
 sleep 120
 echo "Pokracujem"
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f \
-https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/etcd.yaml
-kubectl apply -f \
-https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/calico.yaml
-kubectl apply -f \
-https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/rbac.yaml
+kubectl apply -f rbac.yaml
+kubectl apply -f calico.yaml
 source <(kubectl completion bash)
 echo "source <(kubectl completion bash)" >> ~/.bashrc
 
